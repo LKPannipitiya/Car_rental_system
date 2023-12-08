f4e6de79625d1d26d5a80ead68621d38a7d7309e
DulshanSandeepa <lkpannipitiya99@gmail.com>
2023-12-08 08:41:55 +0530

    update the codes

--- car_rental_project/car_rental_app/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py
-from django import forms
-from .models import SignupData
-
-class SignupForm(forms.ModelForm):
-    class Meta:
-        model = SignupData
-        fields = ['full_name', 'email', 'phone_number']  # Include specific fields
-
